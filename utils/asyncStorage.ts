fb1d43f34fe1126495cb356906a7f96a38db3016
alekssvelvelis <alekssvelvelis@gmail.com>
2025-04-08 16:11:00 +0300

    Cleanup regarding pure black mode

--- utils/asyncStorage.ts
+++ utils/asyncStorage.ts
@@ -3,26 +3,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const THEME_KEY = 'userTheme';
 const READER_OPTIONS_KEY = 'readerOptions';
 const NOVEL_ROWS = 'novelRows';
-const PURE_BLACK_MODE = 'pure-black';
+const PURE_BLACK_MODE = 'isDarkMode';
 
 export const saveItem = async (key: string, value: any) => {
   try {
-    // console.log('b4 json stringify in saveItem', value, typeof value);
     let saveValue = typeof value === 'string' ? value : JSON.stringify(value);
-    // let saveValue = '';
-    // if(typeof value === 'string') {
-    //   console.log('saveItem value is string');
-    //   saveValue =  value;
-    // }else if (typeof value === 'boolean'){
-    //   console.log('saveItem value is boolean');
-    //   saveValue = JSON.stringify(value);
-    // }else{
-    //   console.log('saveItem is neither boolean nor string');
-    // }
     await AsyncStorage.setItem(key, saveValue)
-    // const stringValue = typeof value === 'string' ? value : JSON.stringify(value);
-    // console.log('after stringify in saveItem', stringValue, typeof stringValue);
-    // await AsyncStorage.setItem(key, stringValue);
   } catch (error) {
     console.error(`Error saving ${key}`, error);
   }
@@ -53,15 +39,8 @@ export const getReaderOptions = async (): Promise<string | null> => getItem(READ
 export const saveNovelRows = async(number: string) => saveItem(NOVEL_ROWS, number);
 export const getNovelRows = async(): Promise<string | null> => getItem(NOVEL_ROWS);
 
-export const savePureBlackMode = async (enabled: boolean) => {
-  await saveItem(PURE_BLACK_MODE, enabled);
-};
-
-export const getPureBlackMode = async (): Promise<boolean> =>  getItem(PURE_BLACK_MODE, true);
-
-export const getIsDarkMode = async (): Promise<boolean> => getItem('isDarkMode', true);
-
-export const saveIsDarkMode = async (enabled: boolean) =>  saveItem('isDarkMode', enabled);
+export const saveIsDarkMode = async (enabled: boolean) =>  saveItem(PURE_BLACK_MODE, enabled);
+export const getIsDarkMode = async (): Promise<boolean> => getItem(PURE_BLACK_MODE, true);
 
 export const clearStorage = async () => {
   try {
